ba38a1df06cc6c3143ffc61c87c9f1c94826d963
Gustavo.Sc <91289706+GvT2444@users.noreply.github.com>
2022-06-14 17:08:23 -0300

    rota e html de login criados

--- index.js
+++ index.js
@@ -1,17 +1,2 @@
 const app = require("./src/main");
-const port = normalizaPort(process.env.PORT || "3000");
-
-function normalizaPort(val) {
-  const port = parseInt(val, 10);
-  if (isNaN(port)) {
-    return val;
-  }
-  if (port >= 0) {
-    return port;
-  }
-  return false;
-}
-
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+app.listen(3000);
--- src/router/Router.js
+++ src/router/Router.js
@@ -5,5 +5,6 @@ const router = express.Router();
 const Controller = require('../controllers/Controller');
 
 router.get('/', Controller.listar);
+router.get('/login',Controller.mostralogin);
 
 module.exports = router;
